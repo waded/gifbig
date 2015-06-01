428653597ec081a769f9d29edf09bd53243b680e
Wade Dorrell <wade@dorrells.org>
2015-06-01 14:18:40 -0600

    Now with 100% more XSS

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var https = require('https');
 
 var app = express();
 
-app.use('/', function(req, res) {
-	res.end('gifsmall');
+app.use('/big', function(req, res) {
+	res.end('<html><head><style>body { background-image: url(\'' 
+		+ req.query.url
+		+ '\'); background-repeat:no-repeat; background-size:contain; background-position:center; }</style><body></body></html>');
 });
 
 app.listen(process.env.PORT || 8888);
